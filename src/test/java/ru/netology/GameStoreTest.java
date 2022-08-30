ae72d0ccea9d1691a93f37146a506079ff50eb96
Irina Khvatova <i.chwatowa@mail.ru>
2022-08-30 23:36:48 +0300

    New tests added

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 public class GameStoreTest {
 
-    @Test   //тест позволяет добавить игру в каталог - не находит в каталоге добавленной игры
+    @Test   //тест позволяет добавить игру в каталог - не находит в каталоге добавленной игры - issue 9
     public void shouldAddGame() {
 
         GameStore store = new GameStore();
@@ -16,7 +16,26 @@ public class GameStoreTest {
         assertTrue(store.containsGame(game));
     }
 
-    @Test   //тест на нахождение общего времени игры для одного игрока - не суммирует время
+    @Test  //тест проверяет, есть ли такая игра в каталоге, тест не проходит - issue 10
+    public void shouldNotAddOldGame() {
+
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game1 = new Game("Нетология Баттл Онлайн", "Аркады", store);
+
+        assertTrue(store.containsGame(game1));
+    }
+
+    @Test   //тест проверяет наличие игры в каталоге, тест проходит - такой игры в каталоге нет
+    public void shouldReturnFalseAddGame() {
+
+        GameStore store = new GameStore();
+        Game game2 = new Game("Half-Life", "Шутеры", store);
+
+        assertFalse(store.containsGame(game2));
+    }
+
+    @Test   //тест на нахождение общего времени игры для одного игрока - не суммирует время - issue 8
     public void shouldAddPlayTime() {
         GameStore store = new GameStore();
         Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
@@ -47,17 +66,16 @@ public class GameStoreTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test    //тест на поиск лучшего игрока, один из игроков играл в 2 разные игры - тест не проходит
-    public void shouldGetMostPlayerWhenPlayedSeveralGames() {
+    @Test  //тест на поиск лучшего игрока из 3х разных игроков, если у 2х игроков одинаковое время - тест проходит
+    public void shouldGetMostPlayerWhenSameHours() {
         GameStore store = new GameStore();
         Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
-        Game game2 = store.publishGame("Neodash", "Аркады");
 
         store.addPlayTime("Petya", 5);
-        store.addPlayTime("Kolya", 3);
-        store.addPlayTime("Kolya", 3);
+        store.addPlayTime("Kolya", 5);
+        store.addPlayTime("Katya", 4);
 
-        String expected = "Kolya";
+        String expected = "Petya";
         String actual = store.getMostPlayer();
 
         Assertions.assertEquals(expected, actual);
@@ -74,7 +92,7 @@ public class GameStoreTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test  //тест на суммирование времени всех игроков, проведенного за играми - тест не проходит
+    @Test  //тест на суммирование времени всех игроков, проведенного за играми - тест не проходит - issue 7
     public void shouldGetSumPlayedTime() {
         GameStore store = new GameStore();
         Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
@@ -88,7 +106,6 @@ public class GameStoreTest {
 
         Assertions.assertEquals(expected, actual);
     }
-
 
     // другие ваши тесты
     //Здесь будут тесты напарника
